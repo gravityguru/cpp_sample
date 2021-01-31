a89fde9b08de9ca2a96601e5070bfe77c53e2713
gravityguru <gravityguru@gmail.com>
2021-01-31 15:53:19 +0530

    Initial Commit

---
+++ hello_test/hello.cpp
@@ -0,0 +1,17 @@
+// After installing VS Code 
+// sudo apt-get install build-essential gdb g++
+
+#include<iostream>
+using namespace std;
+int main()
+
+{
+    cout<< "Hi Guru"<< endl;
+    cout << "Yup.Its the output!!"<< endl;
+    return 0;
+}
+
+// go to run and click on start debugging
+// select the environment as C++(GDB/LLDB)
+// select configurration g++ build and debug active file 
+// https://www.youtube.com/watch?v=BEJUdkPemYY
---
+++ lab1/good_bye.cpp
@@ -0,0 +1,17 @@
+// After installing VS Code 
+// sudo apt-get install build-essential gdb g++
+
+#include<iostream>
+using namespace std;
+int main()
+
+{
+    cout<< "Hi Guru"<< endl;
+    cout << "over and out"<< endl;
+    return 0;
+}
+
+// go to run and click on start debugging
+// select the environment as C++(GDB/LLDB)
+// select configurration g++ build and debug active file 
+// https://www.youtube.com/watch?v=BEJUdkPemYY

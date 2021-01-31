9008fc63d5b23c73608790cd04b657759181698a
gravityguru <gravityguru@gmail.com>
2021-01-31 15:56:31 +0530

    running_using_terminal

---
+++ running_c_in_terminal/hello.c
@@ -0,0 +1,19 @@
+//go to work folder and type gedit in the terminal
+
+
+#include<math.h>
+#include<stdio.h>
+
+int main()
+{
+
+printf("Hi Guru\n");
+return 0;
+
+}
+
+//gcc -o test filename.c
+// ./test
+// 
+// the above Command will run your C program
+// https://www.youtube.com/watch?v=A7Ny-ro1hT0
---
+++ running_c_in_terminal/hello.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{
+    cout<< "Hi Guru"<< endl;
+    cout << "Yup.Its the output!!"<< endl;
+    return 0;
+}
+
+//g++ -o test filename.cpp
+// ./test
